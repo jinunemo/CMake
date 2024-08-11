61fa25daef04bd5be4f8caa7fd906134bccd1704
Jinwoo Kim <jinunemo@gmail.com>
2024-08-11 13:01:23 +0900

    include adder library

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <adder.h>
 #include <OLASConfig.h>
 
 int main(int argc, char* argv[]) 
 { 
     std::cout << "Hi" << std::endl; 
+    std::cout << adder::add(72.1f, 73.8f) << std::endl; 
+    
     std::cout << argv[0] << " Version " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << std::endl;
     return 0;
 }
