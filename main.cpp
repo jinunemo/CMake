d644f3eb9a099bfc29e9e236d46a0b6fa55fe8d3
Jinwoo Kim <jinunemo@gmail.com>
2024-08-11 13:41:09 +0900

    make libs optional

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
-#include <adder.h>
 #include <OLASConfig.h>
 
 int main(int argc, char* argv[]) 
 { 
     std::cout << "Hi" << std::endl; 
-    std::cout << adder::add(72.1f, 73.8f) << std::endl; 
-    
+
+#ifdef USE_ADDER
+    std::cout << "using Adder lib:" << adder::add(72.1f, 73.8f) << std::endl; 
+#else
+    std::cout << "NOT USING Adder lib:" << 72.1f + 73.8f << std::endl; 
+#endif
+
     std::cout << argv[0] << " Version " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << std::endl;
     return 0;
 }
