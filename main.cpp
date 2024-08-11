00f9b789f671d37660e978b485e4c68030b2e761
Jinwoo Kim <jinunemo@gmail.com>
2024-08-11 11:38:19 +0900

    source control

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
-int main() { std::cout << "Hi" << std::endl; return 0;}
+#include <OLASConfig.h>
+
+int main(int argc, char* argv[]) 
+{ 
+    std::cout << "Hi" << std::endl; 
+    std::cout << argv[0] << " Version " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << std::endl;
+    return 0;
+}
